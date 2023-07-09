1fbb64f06598d92f9ad52a38ab43a622aba9e70c
Kasturi <75318338+KasturiS899@users.noreply.github.com>
2023-07-09 13:10:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	hasKey("red");
-	hasKey("blue");
-	hasKey("white");
+	const key="blue";
+	const key="red;"
+	const key="white"
 	
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+//const key = prompt("Enter Key.");
 alert(hasKey(key));
